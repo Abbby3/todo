2db8658f367f952f7f27e02db177eaf85d93e629
Abbby3 <ihfmx3@outlook.com>
2024-05-31 07:14:35 +1000

    form and list components + cors workaround

--- frontend/todo/src/App.tsx
+++ frontend/todo/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.scss";
+import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
     <div className="todo">
       <h1 className="title">To Do List</h1>
-      
+      <TaskForm mode={"edit"} id={"0"}/>
     </div>
   );
 }
